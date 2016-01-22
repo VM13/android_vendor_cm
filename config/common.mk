5f48d6727e09676774305d0e4df24da70ad1af08
cryptickid <cryptickid@gmail.com>
2016-01-22 15:44:48 -0500

    updates

--- config/common.mk
+++ config/common.mk
@@ -117,6 +117,46 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# V4A files
+PRODUCT_COPY_FILES += \
+    vendor/screwd/proprietary/V4A/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    vendor/screwd/proprietary/V4A/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libaudiopreprocessing.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libbundlewrapper.so:system/lib/soundfx/libbundlewrapper.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libdownmix.so:system/lib/soundfx/libdownmix.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libeffectproxy.so:system/lib/soundfx/libeffectproxy.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libfmas.so:system/lib/soundfx/libfmas.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libldnhncr.so:system/lib/soundfx/libldnhncr.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libqcomvoiceprocessing.so:system/lib/soundfx/libqcomvoiceprocessing.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libreverbwrapper.so:system/lib/soundfx/libreverbwrapper.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \
+    vendor/screwd/proprietary/V4A/lib/soundfx/libvisualizer.so:system/lib/soundfx/libvisualizer.so \
+    vendor/screwd/proprietary/V4A/priv-app/ViPER4Android/ViPER4Android.apk:system/priv-app/ViPER4Android/ViPER4Android.apk \
+    vendor/screwd/proprietary/V4A/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    
+ # V4A build.prop
+ PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=false \
+    lpa.releaselock=false \
+    lpa.use-stagefright=false \
+    tunnel.decode=false   
+
+# Omni Switch
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/OmniSwitch/org.omnirom.omniswitch.apk:system/priv-app/OmniSwitch/org.omnirom.omniswitch.apk
+
+# OTA Updater
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/OTA-Updater/OTA-Updater-2.0.5.apk:system/app/OTA-Updater/OTA-Updater-2.0.5.apk
+
+# OTA Prop
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/system/rom.ota.prop:system/rom.ota.prop
+
+# OTA Updater feature file
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/system/etc/permissions/com.otaudater.feature.xml:system/etc/permissions/com.otaudater.feature.xml
+
 # This is CM!
 PRODUCT_COPY_FILES += \
     vendor/cm/config/permissions/com.cyanogenmod.android.xml:system/etc/permissions/com.cyanogenmod.android.xml
@@ -230,7 +270,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
 PRODUCT_VERSION_MAJOR = 13
-PRODUCT_VERSION_MINOR = 0.1
+PRODUCT_VERSION_MINOR = 0.1c
 PRODUCT_VERSION_MAINTENANCE = 0-RC0
 
 # Set CM_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
